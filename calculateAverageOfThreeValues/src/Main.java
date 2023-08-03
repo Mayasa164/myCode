ded384adf303c824e1af2dec1b40dcf60cfc5675
Mayasa164 <139007766+Mayasa164@users.noreply.github.com>
2023-08-03 14:18:03 +0400

    calculate Average Of Three Values

---
+++ calculateAverageOfThreeValues/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner read1 = new Scanner(System.in);
+        System.out.println("Enter three values to calculate their average :");
+        double num1 = read.nextDouble();
+        double num2 = read.nextDouble();
+        double num3 = read.nextDouble();
+        System.out.println("Average = "+ (num1 + num2 + num3 )/3);
+        System.out.println("");
+    }
+}
